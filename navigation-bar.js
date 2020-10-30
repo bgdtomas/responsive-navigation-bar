605ef9c868e65abce1f49c8dec2f17338fd35870
bgdtomas <71855224+bgdtomas@users.noreply.github.com>
2020-10-30 12:04:07 -0300

    Adicion de metodos para ocultar el contenido de la pagina.

--- navigation-bar.js
+++ navigation-bar.js
@@ -2,7 +2,13 @@ function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
       x.className += " responsive";
+      document.body.style.overflow = "hidden";
+      document.getElementById("MyContent").style.display = "none";
+      document.getElementById("MyArticle").style.display = "none";
     } else {
       x.className = "topnav";
+      document.body.style.overflow = "scroll";
+      document.getElementById("MyContent").style.display = "block";
+      document.getElementById("MyArticle").style.display = "block";
     }
   }
