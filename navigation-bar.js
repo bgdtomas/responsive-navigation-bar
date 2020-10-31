68d5e023ae09f0ac8606291fe72853b045d5d0ae
bgdtomas <71855224+bgdtomas@users.noreply.github.com>
2020-10-31 14:06:36 -0300

    Code Finalization.

--- navigation-bar.js
+++ navigation-bar.js
@@ -1,14 +1,15 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-      document.body.style.overflow = "hidden";
-      document.getElementById("MyContent").style.display = "none";
-      document.getElementById("MyArticle").style.display = "none";
-    } else {
-      x.className = "topnav";
-      document.body.style.overflow = "scroll";
-      document.getElementById("MyContent").style.display = "block";
-      document.getElementById("MyArticle").style.display = "block";
-    }
+function myFunction(button) {
+  var x = document.getElementById("myTopnav");
+  button.classList.toggle("change")
+  if (x.className === "topnav") {
+    x.className += " responsive";
+    document.body.style.overflow = "hidden";
+    document.getElementById("MyContent").style.display = "none";
+    document.getElementById("MyArticle").style.display = "none";
+  } else {
+    x.className = "topnav";
+    document.body.style.overflow = "scroll";
+    document.getElementById("MyContent").style.display = "block";
+    document.getElementById("MyArticle").style.display = "block";
   }
+}
